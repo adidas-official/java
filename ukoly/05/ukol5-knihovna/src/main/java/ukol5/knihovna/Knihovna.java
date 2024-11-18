10a23e7e2f7d3671a1332f5a06857a6a51cf8d29
zf <zdenek.frydryn@gmail.com>
2024-11-18 23:32:09 +0100

    WIP - ukol5

--- ukoly/05/ukol5-knihovna/src/main/java/ukol5/knihovna/Knihovna.java
+++ ukoly/05/ukol5-knihovna/src/main/java/ukol5/knihovna/Knihovna.java
@@ -1,17 +1,14 @@
 package ukol5.knihovna;
 import java.lang.Math;
 import java.util.Scanner;
+import java.time.LocalDate;
 
 public class Knihovna {
 
-    private int cislo;
-    private double[] mocniny;
-    private double[] odmocniny;
+    private Scanner scanner;
 
-    public Knihovna(int cislo) {
-        this.cislo = cislo;
-        this.mocniny = new double[2];
-        this.odmocniny = new double[2];
+    public Knihovna() {
+        this.scanner = new Scanner(System.in);
     }
 
     public void print_menu() {
@@ -23,74 +20,96 @@ public class Knihovna {
     }
 
     public int get_user_input() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int user_input = scanner.nextInt();
-            return user_input;
-        }
+        return scanner.nextInt();
     }
 
-    public void run() {
-        if (this.cislo == 0) {
-            System.out.println("Cislo neni nastaveno. Ukonceni programu.");
-            return;
+    public String get_mocnina(double cislo, int mocnina) {
+        return String.format("Mocnina (%d) cisla %.2f je %.2f", mocnina, cislo, mocnina_cisla(cislo, mocnina));
+    }
+
+    public String get_odmocnina(double cislo, int odmocnina) {
+        return String.format("Odmocnina (%d) cisla %.2f je %.2f", odmocnina, cislo, odmocnina_cisla(cislo, odmocnina));
+    }   
+
+    public static double mocnina_cisla(double cislo, int mocnina) {
+        return Math.pow(cislo, mocnina);
+    }
+
+    public static double odmocnina_cisla(double cislo, int odmocnina) {
+        return Math.pow(cislo, 1.0/odmocnina);
+    }
+
+    public static void goniometricke_funkce(double uhel) {
+        System.out.printf("%-15s%-15s%-15s%-15s%-15s%n", "Uhel", "Sinus", "Kosinus", "Tangens", "Cotangens");
+        System.out.printf("%-15.2f%-15.2f%-15.2f%-15.2f%-15.2f%n", uhel, Math.sin(uhel), Math.cos(uhel), Math.tan(uhel), 1 / Math.tan(uhel));
+    }
+
+    public static void trojuhelnik_pravouhly(double a, double b) {
+        System.out.println("\nPravouhly trojuhelnik");
+        double prepona = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        double obvod = a + b + prepona;
+        double obsah = (a * b) / 2;
+        double u_a_prepona = Math.asin(a / prepona);
+        double u_b_prepona = Math.asin(b / prepona);
+        // System.out.printf("%-25s%-25s%-25s%-25s%-25s%n", "Obvod trojuhelniku", "Obsah trojuhelniku", "Prepona trojuhelniku", "Uhel mezi preponou a stranou a", "Uhel mezi preponou a stranou b");
+        // System.out.printf("%-25.2f%-25.2f%-25.2f%-25.2f%-25.2f%n", obvod, obsah, prepona, Math.toDegrees(u_a_prepona), Math.toDegrees(u_b_prepona));
+        System.out.printf("%s%10s%10s%10s%10s\n", "Obvod" , "Obsah", "Prepona", "Uhel Alfa", "Uhel Beta");
+        System.out.printf("%-10.2f%-10.2f%-10.2f%-10.2f%-10.2f\n", obvod, obsah, prepona, Math.toDegrees(u_a_prepona), Math.toDegrees(u_b_prepona));
+    }
+
+    public static void trojuhelnik_obecny(double a, double b, double c) {
+        System.out.println("\nObecny trojuhelnik");
+        double obvod = a + b + c;
+        double s = obvod / 2;
+        double obsah = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        double u_a = Math.acos((Math.pow(b, 2) + Math.pow(c, 2) - Math.pow(a, 2)) / (2 * b * c));
+        double u_b = Math.acos((Math.pow(a, 2) + Math.pow(c, 2) - Math.pow(b, 2)) / (2 * a * c));
+        double u_c = Math.acos((Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b));
+        System.out.printf("%-20s%-20s%-20s%-20s%-20s%n", "Strana a", "Strana b", "Strana c", "Obvod", "Obsah");
+        System.out.printf("%-20.2f%-20.2f%-20.2f%-20.2f%-20.2f%n", a, b, c, obvod, obsah);
+        System.out.printf("%-20s%-20s%-20s%n", "Uhel Alfa", "Uhel Beta", "Uhel Gama");
+        System.out.printf("%-20.2f%-20.2f%-20.2f%n", Math.toDegrees(u_a), Math.toDegrees(u_b), Math.toDegrees(u_c));
+    }
+
+    public static void platnost_datumu(int den, int mesic, int rok) {
+        System.out.println(isDateValid(den, mesic, rok));
+    }
+
+    public void aktualni_datum() {
+        System.out.println("Dnes je " + LocalDate.now());
+    }   
+
+    public static boolean isDateValid(int day, int month, int year) {
+        if (year < 1 || month < 1 || month > 12 || day < 1 || day > 31) {
+            return false;
         }
-        int user_input = 0;
-        while (user_input != 4) {
-            print_menu();
-            user_input = get_user_input();
-            switch (user_input) {
-                case 1:
-                    System.out.println("Zadej cislo: ");
-                    this.cislo = get_user_input();
-                    break;
-                case 2:
-                    mocnina_cisla();
-                    break;
-                case 3:
-                    odmocnina_cisla();
-                    break;
-                case 4:
-                    System.out.println("Konec");
-                    break;
-                default:
-                    System.out.println("Neplatna volba");
-                    break;
+        if (month == 2) {
+            if (isLeapYear(year)) {
+                return day <= 29;
+            } else {
+                return day <= 28;
             }
         }
-    }
-
-    public void mocnina_cisla() {
-        for (int i = 0; i < 2; i++) {
-            this.mocniny[i] = Math.pow(this.cislo, i+2);
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return day <= 30;
         }
+        return true;
     }
 
-    public void odmocnina_cisla() {
-        this.odmocniny[0] = Math.sqrt(this.cislo);
-        this.odmocniny[1] = Math.cbrt(this.cislo);
-    }
-
-    public String get_mocniny_table() {
-
-        // return table of powers (mocniny) of the number
-        // print the number itself in first column
-        // print the power of 2 an 3 in the second column
-        // then print the square root and cubic root of the number in the third column
-        // return the table as a string
-
-        String output = String.format("%-10s %-10s %-10s\n", "Cislo " + this.cislo, "Mocniny", "Odmocniny");
-        for (int i = 0; i < 2; i++) {
-            output += String.format("%-10s %-10.2f %-10.2f\n", "", this.mocniny[i], this.odmocniny[i]);
+    private static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            }
+            return true;
         }
-
-        return output;
-
+        return false;
     }
 
     public static void main(String[] args) {
-        int cislo = 27;
-        Knihovna knihovna = new Knihovna(cislo);
-        System.out.println(knihovna.get_mocniny_table());
+        Knihovna.goniometricke_funkce(30);
+        Knihovna.trojuhelnik_pravouhly(3, 4);
+        Knihovna.trojuhelnik_obecny(3, 4, 5);
     }
 
 
